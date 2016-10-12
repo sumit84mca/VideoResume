b9639caacb1699446de992cf108331ffaac75733
sumit84mca <sumit84.mca@gmail.com>
2016-10-12 15:25:39 +0530

    Just Created the soln and this is very first Commit.

---
+++ VideoResume/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideoResume.MvcApplication" Language="C#" %>
---
+++ VideoResume/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoResume.Startup))]
+namespace VideoResume
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
