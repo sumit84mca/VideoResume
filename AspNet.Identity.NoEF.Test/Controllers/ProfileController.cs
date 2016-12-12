a49fa20d21852a12501d784b219f97d1213df697
Sahil Tyagi Sahil Tyagi <sahil.tyagi@sifycorp.com>
2016-12-12 11:07:22 +0530

    updated profile index screen

---
+++ AspNet.Identity.NoEF.Test/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+﻿using AspNet.Identity.NoEF.Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNet.Identity.NoEF.Test.Controllers
+{
+    public class ProfileController : AfterLoginBaseController
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProfileIndex()
+        {
+            var model = new RegisterViewModel
+            {
+                Name = UserSession.Name,
+                MobileNo = UserSession.Mobile,
+                Email = UserSession.Email
+            };
+            return View(model);
+        }
+    }
+}
